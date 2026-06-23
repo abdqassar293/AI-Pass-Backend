dc425e90e5d333bb3024f8966c4848a883ea1a03
AbedAllah Alkassar <abd.kassar293@gmail.com>
2026-06-24 01:20:03 +0200

    Configure CORS in SecurityFilterChain

--- src/main/java/com/HOPn/AI_Pass/security/SecurityConfig.java
+++ src/main/java/com/HOPn/AI_Pass/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
